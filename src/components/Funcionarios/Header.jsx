a1d6a8392de323c23ba56cd9016f1a7771cfd0db
jpsousas <joao.sousa@acad.ufsm.br>
2024-12-03 15:50:25 -0300

    adicionado diretorio Horarios

--- src/components/Funcionarios/Header.jsx
+++ src/components/Funcionarios/Header.jsx
@@ -21,24 +21,30 @@ export function Header ({}){
                     <form className='formulario-body'>
                         
                         <h3>Insira o nome do funcionario:</h3>
-                        <input type='text' placeholder='Ex: Ricardo Fajardo...'></input>
+                        <input type='text' placeholder='Ex: Ricardo Fajardo...' maxLength='100' minLength='20'></input>
                         <h3>Selecione o Cargo:</h3>
                         <select name='cargos'>
                             <option value='1'>Desenvolvedor WEB Jr.</option>
-                            <option value='2'>Estagiario</option>
+                            <option value='2'>Estagiário</option>
                             <option value='3'>QA</option>
                             <option value='4'>Designer</option>
                             <option value='5' selected>Game DEV Jr.</option>
                             <option value='6'>Gerente de Projetos</option>
-                            <option value=''>Outro...</option>
+                            <option value='7'>Analista de Dados</option>
+                            <option value='8'>Engenheiro de Software</option>
+                            <option value='9'>Especialista em Redes</option>
+                            <option value='10'>Desenvolvedor Mobile</option>
+                            <option value='11'>Analista de Suporte</option>
                         </select>
-                        <input type='text' placeholder='Digite o cargo'></input>
-                        <h3>Quais os dias laborais desse colaborador?</h3>
-                        <input></input>
-                        <h3>horario de chegada</h3>
-                        <input></input>
-                        <h3>horario de saida</h3>
-                        <input></input>
+                        <input style={{display: 'none'}} type='text' placeholder='Digite o cargo'></input>
+                        <h3>Qual a jornada de trabalho?</h3> 
+                        <select style={{width: ''}}>
+                            <option value='20'>20h</option>
+                            <option value='25'>25h</option>
+                            <option value='30'>30h</option>
+                            <option value='40'>40h</option>
+                            <option value='44'>44h</option>
+                        </select>
                     </form>
                 </body>
                 <footer className='modal_footer'>
