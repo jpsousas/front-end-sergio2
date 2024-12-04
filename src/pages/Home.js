e74d8c29c320bc12218efd4f67a7be94d3b43ee9
AllequiZ <henriqueamc2003@gmail.com>
2024-12-04 12:47:58 -0300

    Sidebar ajustada, falta header do joão

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,38 @@ import React from 'react';
 import Header from '../components/Header';
 import { TableFuncionarios } from '../components/Funcionarios/TableFuncionarios.jsx';
 
-export default function Home () {
-    return(
-    <div>
+export default function Home() {
+    return (
         <div>
-    <Header />
-        </div>
+            {/* Sidebar */}
+            <div
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '250px',
+                    height: '100%',
+                    backgroundColor: '#333',
+                    zIndex: 1000,
+                    boxShadow: '2px 0 5px rgba(0, 0, 0, 0.5)',
+                }}
+            >
+                <Header />
+            </div>
 
-        <div className="ag-theme-quartz-dark"style={{height: 1000}}>
-            <TableFuncionarios />
+            {/* Tabela */}
+            <div
+                className="table-container"
+                style={{
+                    marginLeft: '250px', // Adiciona espaço para a sidebar
+                    position: 'relative',
+                    zIndex: 1,
+                }}
+            >
+                <div className="ag-theme-quartz-dark" style={{ height: 1000 }}>
+                    <TableFuncionarios />
+                </div>
+            </div>
         </div>
-    </div>
     );
 }
