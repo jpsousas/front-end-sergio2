974538ce419fbad14149ee8d262a349fbdfb0c0b
AllequiZ <henriqueamc2003@gmail.com>
2024-12-06 21:56:38 -0300

    Apaguei 5 linhas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import {HeaderFuncionarios} from './components/Funcionarios/Header.jsx';
-import {HeaderHorarios} from './components/Horarios/Header.jsx';
-import {TableFuncionarios} from './components/Funcionarios/TableFuncionarios.jsx';
-import {TableHorarios} from './components/Horarios/TableHorarios.jsx';
-import {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Schedule from './pages/Schedule';
